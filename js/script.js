12a9b205f76c174bc5b24e73ca58b54c2dbd5fdd
Joao Felipe Zamboni Ferreira De Moura <joao.moura@i9atech.com>
2024-01-26 09:17:13 -0300

    segundo commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageTextarea = document.querySelector("#message");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (nameInput.value === "") {
+        alert("Por favor insira seu nome");
+        return;
+    }
+
+    if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("Por favor coloque um email válido");
+        return;
+    }
+
+    if (messageTextarea.value === "") {
+        alert("Insira uma mensagem por favor");
+        return;
+    }
+
+    form.submit();
+});
+
+function isEmailValid(email) {
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    return emailRegex.test(email);
+}
+
